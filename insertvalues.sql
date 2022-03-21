efee69a58da569b6bab1239e7f4a1b3520e8bc91
Hitarthi Gandhi <56992078+HitarthiGandhi@users.noreply.github.com>
2022-03-21 15:07:35 -0600

    Update insertvalues.sql

--- insertvalues.sql
+++ insertvalues.sql
@@ -1,8 +1,8 @@
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('Pushpak',1);
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('Nayan',2);
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('Chait',3);
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('Rutuja',4);
-INSERT INTO priarworld (firstname, rollnumber) VALUES ('Tanmay',5);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Keyur',1);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Brijal',2);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Hitarthi',3);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Dharmik',4);
+INSERT INTO priarworld (firstname, rollnumber) VALUES ('Charmi',5);
 INSERT INTO priarworld (firstname, rollnumber) VALUES ('B1',1003);
 INSERT INTO priarworld (firstname, rollnumber) VALUES ('B2',1104);
 INSERT INTO priarworld (firstname, rollnumber) VALUES ('B3',1005);
